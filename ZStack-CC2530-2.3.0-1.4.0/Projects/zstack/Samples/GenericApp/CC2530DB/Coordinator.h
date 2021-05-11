181de34cf75ac28cbf416d1502035d70e5afd880
cl <1144688414@qq.com>
2021-05-11 15:07:40 +0800

    sss

--- ZStack-CC2530-2.3.0-1.4.0/Projects/zstack/Samples/GenericApp/CC2530DB/Coordinator.h
+++ ZStack-CC2530-2.3.0-1.4.0/Projects/zstack/Samples/GenericApp/CC2530DB/Coordinator.h
@@ -1,31 +1,28 @@
-#ifndef _COORDINATOR_H  
-#define _COORDINATOR_H
+#ifndef COORDINATOR_H
+#define COORDINATOOR_H
 
-/* 头文件 ----------------------------------------------------------------*/
 #include "ZComDef.h"
-/* 宏定义 ----------------------------------------------------------------*/
-#define GENERICAPP_ENDPOINT        10
 
-#define GENERICAPP_PROFID          0x0F04
+#define GENERICAPP_ENDPOINT 10
 
-#define GENERICAPP_DEVICEID        0x0001
+#define GENERICAPP_PROFID 0X0F04
 
-#define GENERICAPP_DEVICE_VERSION  0
+#define GENERICAPP_DEVICEID 0X0001
 
-#define GENERICAPP_FLAGS           0
+#define GENERICAPP_DEVICE_VERSION 0
 
-#define GENERICAPP_MAX_CLUSTERS    1
+#define GENERICAPP_FLAGS 0
 
-#define GENERICAPP_CLUSTERID       1
+#define GENERICAPP_MAX_CLUSTERS 1
 
-/* 结构体或枚举 ----------------------------------------------------------*/
-/* 函数声明---------------------------------------------------------------*/
-void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pckt ); //消息处理函数
-void GenericApp_SendTheMessage( void );                      //数据发送函数
-/* 外部变量引用 ----------------------------------------------------------*/
-extern void GenericApp_Init ( byte task_id );
+#define GENERICAPP_CLUSTERID 1
 
-extern UINT16 GenericApp_ProcessEvent ( byte task_id,UINT16 events );
+void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pckt );
+void GenericApp_SendTheMessage( void);
 
-#endif // _COORDINATOR_H   
 
+extern void GenericApp_Init(byte task_id);
+
+extern UINT16 GenericApp_ProcessEvent(byte task_id,UINT16 events);
+
+#endif
