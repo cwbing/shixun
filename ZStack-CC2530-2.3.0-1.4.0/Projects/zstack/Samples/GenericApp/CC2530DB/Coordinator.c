9305b17004f24e933e9e01062d5b70726f48ae43
e <1141606265@qq.com>
2021-05-11 15:14:35 +0800

    协调器

--- ZStack-CC2530-2.3.0-1.4.0/Projects/zstack/Samples/GenericApp/CC2530DB/Coordinator.c
+++ ZStack-CC2530-2.3.0-1.4.0/Projects/zstack/Samples/GenericApp/CC2530DB/Coordinator.c
@@ -14,11 +14,30 @@
 #include "hal_led.h"
 #include "hal_key.h"
 #include "hal_uart.h"
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+* 文件名  ： GENERICAPP_MAX_CLUSTERS
+* 作者    ： chenweibing
+* 版本    ： V1.0.0
+* 时间    ： 2021/5/11
+* 简要    ： GENERICAPP_MAX_CLUSTERS宏定义  
+********************************************************************
+* 副本
+*
+*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 
 const cId_t GenericApp_ClusterList[GENERICAPP_MAX_CLUSTERS] =
 {
 GENERICAPP_CLUSTERID
 };
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名：GenericApp_SimpleDesc
+* 作者 ： chenweibing
+* 参数：void
+* 返回：void
+* 描述：函数说明
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 const SimpleDescriptionFormat_t GenericApp_SimpleDesc =
 {
   GENERICAPP_ENDPOINT,
@@ -32,12 +51,22 @@ const SimpleDescriptionFormat_t GenericApp_SimpleDesc =
   (cId_t *)NULL
 };
 
+
 endPointDesc_t GenericApp_epDesc;
 byte GenericApp_TaskID;
 byte GenericApp_TransID;
 
+
 void GenericApp_MessageMSGCB(afIncomingMSGPacket_t *pckt );
 void GenericApp_SendTheMessage(void);
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名：GenericApp_Init
+* 作者 ： chenweibing
+* 参数：byte task_id
+* 返回：void
+* 描述：函数说明
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 void GenericApp_Init(byte task_id)
 {
@@ -49,6 +78,15 @@ void GenericApp_Init(byte task_id)
   GenericApp_epDesc.latencyReq   =noLatencyReqs;
   afRegister( &GenericApp_epDesc);
 }
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名：GenericApp_ProcessEvent
+* 作者 ： chenweibing
+* 参数： byte task_id, UINT16 events
+* 返回：UINT16 
+* 描述：函数说明
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 
 UINT16 GenericApp_ProcessEvent( byte task_id, UINT16 events )
 {
@@ -74,6 +112,8 @@ UINT16 GenericApp_ProcessEvent( byte task_id, UINT16 events )
   }
   return 0 ;
 }
+
+
 void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pkt)
 {
   unsigned char buffer[4]=" ";
